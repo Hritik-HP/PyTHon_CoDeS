d96d050b973f490584ba1c4b77741cb7618386d6
Hritik Phoolwani <54300161+Hritik-HP@users.noreply.github.com>
2022-10-12 12:27:31 +0530

    Add files via upload

---
+++ shuffle.py
@@ -0,0 +1,8 @@
+import random
+sample_list =['a','b','c','d','e','f','g']
+print("original sample list:")
+print(sample_list)
+
+random.shuffle(sample_list)
+print("\n After the first shuffle")
+print(sample_list)
