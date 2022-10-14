d6a9210ef0b2a592a1d2c00fad4c8187361219b3
Hritik Phoolwani <54300161+Hritik-HP@users.noreply.github.com>
2022-10-14 12:04:25 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,14 @@
+s="Welcome To The Jungle"
+print("length of",s,"is:",len(s))
+
+print(s.lower())   # prints all characters in lower case
+print(s.upper())   # prints all characters in upper case
+
+print(s.swapcase())  # prints all characters in swap
+print(s.capitalize())  # prints all characters in capitalized
+print(s.title())
+print(s.lstrip("Welcome"))  #to remove from left side
+
+
+print("minimum=",min(s))
+print("minimum=",max(s))
