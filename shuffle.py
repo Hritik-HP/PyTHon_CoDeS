8b6c8dfe0960b1e32072549d494c9473e6705f00
Hritik Phoolwani <54300161+Hritik-HP@users.noreply.github.com>
2022-11-09 10:47:57 +0530

    Shuffle
    In order to shuffle the data

--- shuffle.py
+++ shuffle.py
@@ -3,6 +3,7 @@ sample_list =['a','b','c','d','e','f','g']
 print("original sample list:")
 print(sample_list)
 
+
 random.shuffle(sample_list)
 print("\n After the first shuffle")
 print(sample_list)
